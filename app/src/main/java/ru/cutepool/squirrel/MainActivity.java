a0968cbac1725e2ed108823180eddec938a03d43
calmmellivora <andy@cutepool.ru>
2019-03-15 17:34:08 +0300

    Test commit from android studio

--- app/src/main/java/ru/cutepool/squirrel/MainActivity.java
+++ app/src/main/java/ru/cutepool/squirrel/MainActivity.java
@@ -3,6 +3,7 @@ package ru.cutepool.squirrel;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -88,6 +89,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
 
         }
+
+        Log.d("TEST", "Testing git commit from android studio");
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
